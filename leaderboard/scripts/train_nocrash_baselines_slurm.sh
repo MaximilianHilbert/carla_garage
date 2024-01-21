0f98f5aef45af42a7f719bd03ad8cc66e61d3bcc
max <maximilian.hilbert@gmx.de>
2024-01-21 12:23:21 +0100

    changed order in slurm script

--- leaderboard/scripts/train_nocrash_baselines_slurm.sh
+++ leaderboard/scripts/train_nocrash_baselines_slurm.sh
@@ -1,15 +1,15 @@
-export WORK_DIR=/home/maximilian/Master/carla_garage
-export CONFIG_ROOT=${WORK_DIR}/coil_configuration
-export TEAM_CODE=$WORK_DIR/team_code
-export CARLA_ROOT=${WORK_DIR}/carla
-export COIL_NETWORK=${WORK_DIR}/coil_network
-export DATASET_ROOT=/home/maximilian/training_data_split
+# export WORK_DIR=/home/maximilian/Master/carla_garage
+# export CONFIG_ROOT=${WORK_DIR}/coil_configuration
+# export TEAM_CODE=$WORK_DIR/team_code
+# export CARLA_ROOT=${WORK_DIR}/carla
+# export COIL_NETWORK=${WORK_DIR}/coil_network
+# export DATASET_ROOT=/home/maximilian/training_data_split
 
-#export WORK_DIR=/mnt/qb/work/geiger/gwb629/carla_garage
-#export CONFIG_ROOT=${WORK_DIR}/coil_config
-#export CARLA_ROOT=${WORK_DIR}/carla
-#export DATASET_ROOT=/mnt/qb/work2/geiger0/bjaeger25/datasets/hb_dataset_v08_2023_05_10
-#export LD_LIBRARY_PATH="/mnt/qb/work/geiger/gwb629/conda/garage/lib":$LD_LIBRARY_PATH
+export WORK_DIR=/mnt/qb/work/geiger/gwb629/carla_garage
+export CONFIG_ROOT=${WORK_DIR}/coil_config
+export CARLA_ROOT=${WORK_DIR}/carla
+export DATASET_ROOT=/mnt/qb/work2/geiger0/bjaeger25/datasets/hb_dataset_v08_2023_05_10
+export LD_LIBRARY_PATH="/mnt/qb/work/geiger/gwb629/conda/garage/lib":$LD_LIBRARY_PATH
 
 export CARLA_SERVER=${CARLA_ROOT}/CarlaUE4.sh
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
