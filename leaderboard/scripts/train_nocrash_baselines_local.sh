3baf598815301c304af868b365b618382f31651b
max <maximilian.hilbert@gmx.de>
2024-01-22 10:26:53 +0100

    fixed paths

--- leaderboard/scripts/train_nocrash_baselines_local.sh
+++ leaderboard/scripts/train_nocrash_baselines_local.sh
@@ -7,7 +7,7 @@ export COIL_NETWORK=${WORK_DIR}/coil_network
 export DATASET_ROOT=/home/maximilian/training_data_split
 #cluster
 #export WORK_DIR=/mnt/qb/work/geiger/gwb629/carla_garage
-#export CONFIG_ROOT=${WORK_DIR}/coil_config
+#export CONFIG_ROOT=${WORK_DIR}/coil_configuration
 #export TEAM_CODE=$WORK_DIR/team_code
 #export CARLA_ROOT=${WORK_DIR}/carla
 #export DATASET_ROOT=/mnt/qb/work2/geiger0/bjaeger25/datasets/hb_dataset_v08_2023_05_10
