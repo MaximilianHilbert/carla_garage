e7f0ebb06f51cf858fbd9d8f46837db24fd4d317
max <maximilian.hilbert@gmx.de>
2024-01-26 09:52:42 +0100

    moved to tcml cluster; single train vs multi-train introduction

--- leaderboard/scripts/train_nocrash_baselines_single.sh
+++ leaderboard/scripts/train_nocrash_baselines_single.sh
@@ -1,10 +1,28 @@
+#!/bin/sh
+#SBATCH --job-name=reproduce_ARP_arp_vanilla_single
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --time=00-12:00
+#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:A4000:1
+#SBATCH --cpus-per-task=24
+#SBATCH --output=/home/hilbert/slurmlogs/carla_garage/%j.out  # File to which STDOUT will be written
+#SBATCH --error=/home/hilbert/slurmlogs/carla_garage/%j.err   # File to which STDERR will be written
+
 #local
-export WORK_DIR=/home/maximilian/Master/carla_garage
+# export WORK_DIR=/home/maximilian/Master/carla_garage
+# export CONFIG_ROOT=${WORK_DIR}/coil_configuration
+# export TEAM_CODE=$WORK_DIR/team_code
+# export CARLA_ROOT=${WORK_DIR}/carla
+# export COIL_NETWORK=${WORK_DIR}/coil_network
+# export DATASET_ROOT=/home/maximilian/training_data_split
+#cluster tcml
+export WORK_DIR=/home/hilbert/carla_garage
 export CONFIG_ROOT=${WORK_DIR}/coil_configuration
 export TEAM_CODE=$WORK_DIR/team_code
 export CARLA_ROOT=${WORK_DIR}/carla
-export COIL_NETWORK=${WORK_DIR}/coil_network
-export DATASET_ROOT=/home/maximilian/training_data_split
+export DATASET_ROOT=/home/hilbert/datasets/hb_dataset_v08_2023_05_10
+export LD_LIBRARY_PATH="/home/hilbert/miniconda3/envs/garage/lib":$LD_LIBRARY_PATH
 #cluster
 #export WORK_DIR=/mnt/qb/work/geiger/gwb629/carla_garage
 #export CONFIG_ROOT=${WORK_DIR}/coil_configuration
@@ -22,5 +40,9 @@ export PYTHONPATH=$PYTHONPATH:$CONFIG_ROOT
 export PYTHONPATH=$PYTHONPATH:${COIL_NETWORK}
 export PYTHONPATH=$PYTHONPATH:${TEAM_CODE}
 export PYTHONPATH=$PYTHONPATH:${WORK_DIR}
+
+#cluster tcml
+#source ~/.bashrc
+#conda activate /home/hilbert/miniconda3/envs/garage
 
 python $TEAM_CODE/coil_train.py --seed 1 --gpu 0 --baseline_folder_name ARP --baseline_name arp_vanilla --number_of_workers 10 --training_repetition 0 --use-disk-cache 1
