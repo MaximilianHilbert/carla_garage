ef17b13d0a18498426a7a6c5eb90c69fc1d7027d
max <maximilian.hilbert@gmx.de>
2024-01-29 14:56:55 +0100

    changed training pipeline for multiple baselines and tcml cluster

--- train_nocrash_baselines.py
+++ train_nocrash_baselines.py
@@ -1,27 +1,27 @@
 import os
 import subprocess
-def generate_and_place_batch_script(args, seed, repetition):
+def generate_and_place_batch_script(workers, seed, training_repetition, baseline_folder_name, baseline_name):
     job_path=os.path.join(os.environ.get("WORK_DIR"), "job_files")
     os.makedirs(job_path, exist_ok=True)
-    job_full_path=os.path.join(job_path, f"{args.baseline_folder_name}_{args.baseline_name}_{str(repetition)}.sh")
+    job_full_path=os.path.join(job_path, f"{baseline_folder_name}_{baseline_name}_{str(training_repetition)}.sh")
     with open(job_full_path, 'w', encoding='utf-8') as f:
         command=f"""#!/bin/sh
-#SBATCH --job-name=reproduce_{args.baseline_folder_name}_{args.baseline_name}
+#SBATCH --job-name=reproduce_{baseline_folder_name}_{baseline_name}_{training_repetition}
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --time=00-12:00
-#SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task={args.number_of_workers}
-#SBATCH --output=/mnt/qb/work/geiger/gwb629/slurmlogs/carla_garage/%j.out  # File to which STDOUT will be written
-#SBATCH --error=/mnt/qb/work/geiger/gwb629/slurmlogs/carla_garage/%j.err   # File to which STDERR will be written
+#SBATCH --time=03-00:00
+#SBATCH --parition=week
+#SBATCH --gres=gpu:A4000:1
+#SBATCH --cpus-per-task={workers}
+#SBATCH --output=/home/hilbert/slurmlogs/%j.out  # File to which STDOUT will be written
+#SBATCH --error=/home/hilbert/slurmlogs/%j.err   # File to which STDERR will be written
 
-export WORK_DIR=/mnt/qb/work/geiger/gwb629/carla_garage
-export CONFIG_ROOT=$WORK_DIR/coil_configuration
-export CARLA_ROOT=$WORK_DIR/carla
-export DATASET_ROOT=/mnt/qb/work2/geiger0/bjaeger25/datasets/hb_dataset_v08_2023_05_10
-export LD_LIBRARY_PATH="/mnt/qb/work/geiger/gwb629/conda/garage/lib":$LD_LIBRARY_PATH
-export COIL_NETWORK=$WORK_DIR/coil_network
-export TEAM_CODE=$WORK_DIR/team_code
+#export WORK_DIR=/home/hilbert/carla_garage
+#export CONFIG_ROOT=$WORK_DIR/coil_configuration
+#export TEAM_CODE=$WORK_DIR/team_code
+#export CARLA_ROOT=$WORK_DIR/carla
+#export DATASET_ROOT=/home/hilbert/dataset_v08
+#export LD_LIBRARY_PATH="/home/hilbert/miniconda3/envs/garage/lib":$LD_LIBRARY_PATH
 
 
 export CARLA_SERVER=$CARLA_ROOT/CarlaUE4.sh
@@ -34,9 +34,10 @@ export PYTHONPATH=$PYTHONPATH:$COIL_NETWORK
 export PYTHONPATH=$PYTHONPATH:$TEAM_CODE
 export PYTHONPATH=$PYTHONPATH:$WORK_DIR
 
-source ~/.bashrc
-conda activate /mnt/qb/work/geiger/gwb629/conda/garage
-python3 $WORK_DIR/team_code/coil_train.py --gpu {args.gpu} --seed {seed} --training_repetition {repetition} --use-disk-cache {args.use_disk_cache} --baseline_folder_name {args.baseline_folder_name} --baseline_name {args.baseline_name} --number_of_workers {args.number_of_workers}
+source /home/hilbert/.bashrc
+eval "$(conda shell.bash hook)"
+conda activate garage
+python3 $WORK_DIR/team_code/coil_train.py --gpu {args.gpu} --seed {seed} --training_repetition {training_repetition} --use-disk-cache {args.use_disk_cache} --baseline_folder_name {baseline_folder_name} --baseline_name {baseline_name} --number_of_workers {workers}
         """
         f.write(command)
     out=subprocess.check_output(f'chmod u+x {job_full_path}', shell=True)
@@ -44,14 +45,15 @@ python3 $WORK_DIR/team_code/coil_train.py --gpu {args.gpu} --seed {seed} --train
     print(out)
 def main(args):
     for training_repetition, seed in enumerate(args.seeds):
-        generate_and_place_batch_script(args,seed, training_repetition)
+        for baseline_folder_name in args.baseline_folder_names:
+            for baseline_name in args.baseline_names:
+                generate_and_place_batch_script(args.number_of_workers,seed, training_repetition, baseline_folder_name, baseline_name)
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--training_seeds', default=1, required=False)
     parser.add_argument('--gpu', default=0, required=True)
-    parser.add_argument("--baseline_name", default="arp_vanilla", dest='baseline_name',help="")
-    parser.add_argument("--baseline_folder_name", default="ARP", dest='baseline_folder_name',help="")
+    parser.add_argument("--baseline_names", nargs='+', type=str, dest='baseline_names',help="")
+    parser.add_argument("--baseline_folder_names", nargs='+', type=str, dest='baseline_folder_names',help="")
     parser.add_argument('--seeds', nargs='+', type=int, help='List of seed values')
     parser.add_argument('--use-disk-cache', dest="use_disk_cache", type=bool, default=0)
     parser.add_argument(
