abfe66e984f5f3a6db3a4387d38833c5f0bdaa14
max <maximilian.hilbert@gmx.de>
2024-01-30 13:21:17 +0100

    changed keyframes script (test ntasks2)

--- leaderboard/scripts/train_importance_weights_keyframes.sh
+++ leaderboard/scripts/train_importance_weights_keyframes.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 #SBATCH --job-name=reproduce_ARP_arp_vanilla_single
-#SBATCH --ntasks=1
+#SBATCH --ntasks=2
 #SBATCH --nodes=1
 #SBATCH --partition=day
-#SBATCH --time=01-00:00
-#SBATCH --gres=gpu:A4000:1
+#SBATCH --time=00-01:00
+#SBATCH --gres=gpu:1080ti:1
 #SBATCH --mem=100GB
-#SBATCH --cpus-per-task=12
+#SBATCH --cpus-per-task=24
 #SBATCH --output=/home/hilbert/slurmlogs/%j.out  # File to which STDOUT will be written
 #SBATCH --error=/home/hilbert/slurmlogs/%j.err   # File to which STDERR will be written
 
 #local
-export WORK_DIR=/home/maximilian/Master/carla_garage
-export CONFIG_ROOT=${WORK_DIR}/coil_configuration
-export TEAM_CODE=$WORK_DIR/team_code
-export CARLA_ROOT=${WORK_DIR}/carla
-export COIL_NETWORK=${WORK_DIR}/coil_network
-export DATASET_ROOT=/home/maximilian/training_data_split
-#cluster tcml
-# export WORK_DIR=/home/hilbert/carla_garage
+# export WORK_DIR=/home/maximilian/Master/carla_garage
 # export CONFIG_ROOT=${WORK_DIR}/coil_configuration
 # export TEAM_CODE=$WORK_DIR/team_code
 # export CARLA_ROOT=${WORK_DIR}/carla
-# export DATASET_ROOT=/home/hilbert/dataset_v08
-# export LD_LIBRARY_PATH="/home/hilbert/miniconda3/envs/garage/lib":$LD_LIBRARY_PATH
+# export COIL_NETWORK=${WORK_DIR}/coil_network
+# export DATASET_ROOT=/home/maximilian/training_data_split
+#cluster tcml
+export WORK_DIR=/home/hilbert/carla_garage
+export CONFIG_ROOT=${WORK_DIR}/coil_configuration
+export TEAM_CODE=$WORK_DIR/team_code
+export CARLA_ROOT=${WORK_DIR}/carla
+export DATASET_ROOT=/home/hilbert/dataset_v08
+export LD_LIBRARY_PATH="/home/hilbert/miniconda3/envs/garage/lib":$LD_LIBRARY_PATH
 #cluster
 #export WORK_DIR=/mnt/qb/work/geiger/gwb629/carla_garage
 #export CONFIG_ROOT=${WORK_DIR}/coil_configuration
@@ -46,8 +46,8 @@ export PYTHONPATH=$PYTHONPATH:${WORK_DIR}
 # source ~/.bashrc
 # conda activate /mnt/qb/work/geiger/gwb629/conda/garage
 #tcml
-#source /home/hilbert/.bashrc
-#eval "$(conda shell.bash hook)"
-#conda activate garage
+source /home/hilbert/.bashrc
+eval "$(conda shell.bash hook)"
+conda activate garage
 
-python $WORK_DIR/keyframes/get_importance_weights_training.py --seeds 122 --baseline-folder-name keyframes --baseline-name keyframes_vanilla_weights --number-of-workers 12 --use-disk-cache 0
+python $WORK_DIR/keyframes/get_importance_weights_training.py --seeds 122 --baseline-folder-name keyframes --baseline-name keyframes_vanilla_weights --number-of-workers 24 --use-disk-cache 0
