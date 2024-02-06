85988d519acc243ca9adf00aaf34fa7a886e2943
max <maximilian.hilbert@gmx.de>
2024-02-06 10:26:51 +0100

    changed slurm default partition; default batch size to 30

--- train_nocrash_baselines.py
+++ train_nocrash_baselines.py
@@ -14,7 +14,7 @@ def generate_batch_script(args, seed, training_repetition, baseline_folder_name,
 #SBATCH --nodes=1
 #SBATCH --time=2-00:00
 #SBATCH --gres=gpu:1
-#SBATCH --partition=a100-galvani
+#SBATCH --partition=gpu-2080ti,gpu-v100
 #SBATCH --cpus-per-task={args.number_of_workers}
 #SBATCH --mem=100G
 #SBATCH --output=/mnt/qb/work/geiger/gwb629/slurmlogs/%j.out  # File to which STDOUT will be written
