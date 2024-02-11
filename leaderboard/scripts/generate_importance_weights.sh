46ef8a41d1ddb679214e5ce4666e8f17c1453c28
max <maximilian.hilbert@gmx.de>
2024-02-11 18:53:14 +0100

    keyframes first training

--- leaderboard/scripts/generate_importance_weights.sh
+++ leaderboard/scripts/generate_importance_weights.sh
@@ -8,21 +8,30 @@
 #SBATCH --cpus-per-task=25
 #SBATCH --output=/mnt/qb/work/geiger/gwb629/slurmlogs/%j.out  # File to which STDOUT will be written
 #SBATCH --error=/mnt/qb/work/geiger/gwb629/slurmlogs/%j.err   # File to which STDERR will be written
+#uni pc
 
-#export WORK_DIR=/home/maximilian/Master/carla_garage
-#export CONFIG_ROOT=${WORK_DIR}/coil_configuration
-#export TEAM_CODE=$WORK_DIR/team_code
-#export CARLA_ROOT=${WORK_DIR}/carla
-#export COIL_NETWORK=${WORK_DIR}/coil_network
-#export DATASET_ROOT=/home/maximilian/test
 
-export WORK_DIR=/mnt/qb/work/geiger/gwb629/carla_garage
-export CONFIG_ROOT=$WORK_DIR/coil_configuration
-export CARLA_ROOT=$WORK_DIR/carla
-export DATASET_ROOT=/mnt/qb/work2/geiger0/bjaeger25/datasets/hb_dataset_v08_2023_05_10
-export LD_LIBRARY_PATH="/mnt/qb/work/geiger/gwb629/conda/garage/lib":$LD_LIBRARY_PATH
+export WORK_DIR=/home/maximilian-hilbert/carla_garage
+export CONFIG_ROOT=${WORK_DIR}/coil_configuration
 export TEAM_CODE=$WORK_DIR/team_code
+export CARLA_ROOT=${WORK_DIR}/carla
 export COIL_NETWORK=${WORK_DIR}/coil_network
+export DATASET_ROOT=/home/maximilian-hilbert/datasets/transfuser_v08
+#local
+# export WORK_DIR=/home/maximilian/Master/carla_garage
+# export CONFIG_ROOT=${WORK_DIR}/coil_configuration
+# export TEAM_CODE=$WORK_DIR/team_code
+# export CARLA_ROOT=${WORK_DIR}/carla
+# export COIL_NETWORK=${WORK_DIR}/coil_network
+# export DATASET_ROOT=/home/maximilian/test
+
+# export WORK_DIR=/mnt/qb/work/geiger/gwb629/carla_garage
+# export CONFIG_ROOT=$WORK_DIR/coil_configuration
+# export CARLA_ROOT=$WORK_DIR/carla
+# export DATASET_ROOT=/mnt/qb/work2/geiger0/bjaeger25/datasets/hb_dataset_v08_2023_05_10
+# export LD_LIBRARY_PATH="/mnt/qb/work/geiger/gwb629/conda/garage/lib":$LD_LIBRARY_PATH
+# export TEAM_CODE=$WORK_DIR/team_code
+# export COIL_NETWORK=${WORK_DIR}/coil_network
 
 export CARLA_SERVER=$CARLA_ROOT/CarlaUE4.sh
 export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI
@@ -41,5 +50,5 @@ export PYTHONPATH=$PYTHONPATH:$WORK_DIR
 source ~/.bashrc
 conda activate /mnt/qb/work/geiger/gwb629/conda/garage
 
-python $WORK_DIR/keyframes/get_importance_weights_training.py --seeds 111 --training-repetition 0 --baseline-folder-name keyframes --baseline-name keyframes_vanilla_weights --number-of-workers 25 --neurons 300 --batch-size=512
-python $WORK_DIR/keyframes/get_importance_weights_inference.py --training-repetition 0 --baseline-folder-name keyframes --baseline-name keyframes_vanilla_weights --number-of-workers 25
+python $WORK_DIR/keyframes/get_importance_weights_training.py --seeds 111 --training-repetition 0 --baseline-folder-name keyframes --baseline-name keyframes_vanilla_weights --number-of-workers 25 --neurons 300 --batch-size 512
+#python $WORK_DIR/keyframes/get_importance_weights_inference.py --training-repetition 0 --baseline-folder-name keyframes --baseline-name keyframes_vanilla_weights --number-of-workers 25
