5fabbeb9a7b953e7de2ffecee4c4ab8200e4a47c
max <maximilian.hilbert@gmx.de>
2024-03-05 10:38:19 +0100

    hotfix ddp backend port

--- coil_utils/baseline_helpers.py
+++ coil_utils/baseline_helpers.py
@@ -14,7 +14,15 @@ def set_seed(seed):
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
+def find_free_port():
+    """ https://stackoverflow.com/questions/1365265/on-localhost-how-do-i-pick-a-free-port-number """
+    import socket
+    from contextlib import closing
 
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(('', 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return str(s.getsockname()[1])
 
 def merge_with_yaml(transfuser_config_object, baseline_name, experiment):
     with open(
