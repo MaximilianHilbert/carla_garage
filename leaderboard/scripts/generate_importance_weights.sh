67a3459c7a1cce0c1da92ec08b1cc95432583567
max <maximilian.hilbert@gmx.de>
2024-03-12 16:30:13 +0100

    changed sh for keyframes

--- leaderboard/scripts/generate_importance_weights.sh
+++ leaderboard/scripts/generate_importance_weights.sh
@@ -11,12 +11,12 @@
 #uni pc
 
 
-export WORK_DIR=/home/maximilian-hilbert/carla_garage
-export CONFIG_ROOT=${WORK_DIR}/coil_configuration
-export TEAM_CODE=$WORK_DIR/team_code
-export CARLA_ROOT=${WORK_DIR}/carla
-export COIL_NETWORK=${WORK_DIR}/coil_network
-export DATASET_ROOT=/home/maximilian-hilbert/datasets/transfuser_v08
+# export WORK_DIR=/home/maximilian-hilbert/carla_garage
+# export CONFIG_ROOT=${WORK_DIR}/coil_configuration
+# export TEAM_CODE=$WORK_DIR/team_code
+# export CARLA_ROOT=${WORK_DIR}/carla
+# export COIL_NETWORK=${WORK_DIR}/coil_network
+# export DATASET_ROOT=/home/maximilian-hilbert/datasets/transfuser_v08
 #local
 # export WORK_DIR=/home/maximilian/Master/carla_garage
 # export CONFIG_ROOT=${WORK_DIR}/coil_configuration
@@ -25,13 +25,13 @@ export DATASET_ROOT=/home/maximilian-hilbert/datasets/transfuser_v08
 # export COIL_NETWORK=${WORK_DIR}/coil_network
 # export DATASET_ROOT=/home/maximilian/test
 
-# export WORK_DIR=/mnt/qb/work/geiger/gwb629/carla_garage
-# export CONFIG_ROOT=$WORK_DIR/coil_configuration
-# export CARLA_ROOT=$WORK_DIR/carla
-# export DATASET_ROOT=/mnt/qb/work2/geiger0/bjaeger25/datasets/hb_dataset_v08_2023_05_10
-# export LD_LIBRARY_PATH="/mnt/qb/work/geiger/gwb629/conda/garage/lib":$LD_LIBRARY_PATH
-# export TEAM_CODE=$WORK_DIR/team_code
-# export COIL_NETWORK=${WORK_DIR}/coil_network
+export WORK_DIR=/mnt/qb/work/geiger/gwb629/carla_garage
+export CONFIG_ROOT=$WORK_DIR/coil_configuration
+export CARLA_ROOT=$WORK_DIR/carla
+export DATASET_ROOT=/mnt/qb/work2/geiger0/bjaeger25/datasets/hb_dataset_v08_2023_05_10
+export LD_LIBRARY_PATH="/mnt/qb/work/geiger/gwb629/conda/garage/lib":$LD_LIBRARY_PATH
+export TEAM_CODE=$WORK_DIR/team_code
+export COIL_NETWORK=${WORK_DIR}/coil_network
 
 export CARLA_SERVER=$CARLA_ROOT/CarlaUE4.sh
 export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI
@@ -47,8 +47,8 @@ export PYTHONPATH=$PYTHONPATH:$WORK_DIR
 # eval "$(conda shell.bash hook)"
 # conda activate garage
 
-source ~/.bashrc
-conda activate /mnt/qb/work/geiger/gwb629/conda/garage
+# source ~/.bashrc
+# conda activate /mnt/qb/work/geiger/gwb629/conda/garage
 
-python $WORK_DIR/keyframes/get_importance_weights_training.py --seeds 111 --training-repetition 0 --baseline-folder-name keyframes --experiment keyframes_weights --number-of-workers 12 --neurons 300 --batch-size 512 --setting all
-python $WORK_DIR/keyframes/get_importance_weights_inference.py --training-repetition 0 --baseline-folder-name keyframes --experiment keyframes_weights --number-of-workers 12 --setting all
+python $WORK_DIR/keyframes/get_importance_weights_training.py --seeds 111 --training-repetition 0 --baseline-folder-name keyframes --experiment keyframes_weights --number-of-workers 8 --neurons 300 --batch-size 512 --setting 02_withheld
+python $WORK_DIR/keyframes/get_importance_weights_inference.py --training-repetition 0 --baseline-folder-name keyframes --experiment keyframes_weights --number-of-workers 8 --setting 02_withheld
