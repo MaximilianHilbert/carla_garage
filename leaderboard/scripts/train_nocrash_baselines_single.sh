d47747b6f459761c41c53f0dfe909bb95b923e3b
max <maximilian.hilbert@gmx.de>
2024-04-11 16:39:36 +0200

    changed defaults

--- leaderboard/scripts/train_nocrash_baselines_single.sh
+++ leaderboard/scripts/train_nocrash_baselines_single.sh
@@ -26,21 +26,21 @@
 # export LD_LIBRARY_PATH="/home/hilbert/miniconda3/envs/garage/lib":$LD_LIBRARY_PATH
 
 #uni pc
-# export WORK_DIR=/home/maximilian-hilbert/carla_garage
-# export CONFIG_ROOT=${WORK_DIR}/coil_configuration
-# export CARLA_ROOT=${WORK_DIR}/carla
-# export DATASET_ROOT=/home/maximilian-hilbert/datasets/transfuser_v08
-# export LD_LIBRARY_PATH="/mnt/qb/work/geiger/gwb629/conda/garage/lib":$LD_LIBRARY_PATH
-# export TEAM_CODE=$WORK_DIR/team_code
-# export COIL_NETWORK=${WORK_DIR}/coil_network
-#mlcloud
-export WORK_DIR=/mnt/qb/work/geiger/gwb629/carla_garage
+export WORK_DIR=/home/maximilian-hilbert/carla_garage
 export CONFIG_ROOT=${WORK_DIR}/coil_configuration
 export CARLA_ROOT=${WORK_DIR}/carla
-export DATASET_ROOT=/mnt/qb/work2/geiger0/bjaeger25/datasets/hb_dataset_v08_2023_05_10
+export DATASET_ROOT=/home/maximilian-hilbert/datasets/tf_dataset
 export LD_LIBRARY_PATH="/mnt/qb/work/geiger/gwb629/conda/garage/lib":$LD_LIBRARY_PATH
 export TEAM_CODE=$WORK_DIR/team_code
 export COIL_NETWORK=${WORK_DIR}/coil_network
+#mlcloud
+# export WORK_DIR=/mnt/qb/work/geiger/gwb629/carla_garage
+# export CONFIG_ROOT=${WORK_DIR}/coil_configuration
+# export CARLA_ROOT=${WORK_DIR}/carla
+# export DATASET_ROOT=/mnt/qb/work2/geiger0/bjaeger25/datasets/hb_dataset_v08_2023_05_10
+# export LD_LIBRARY_PATH="/mnt/qb/work/geiger/gwb629/conda/garage/lib":$LD_LIBRARY_PATH
+# export TEAM_CODE=$WORK_DIR/team_code
+# export COIL_NETWORK=${WORK_DIR}/coil_network
 
 export CARLA_SERVER=${CARLA_ROOT}/CarlaUE4.sh
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
