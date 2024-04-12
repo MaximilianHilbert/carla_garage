9aeaae90dfc44f92903a32c4970fb4eb68588875
max <maximilian.hilbert@gmx.de>
2024-04-12 13:01:33 +0200

    changed sh default values

--- leaderboard/scripts/visualize_copycat.sh
+++ leaderboard/scripts/visualize_copycat.sh
@@ -56,6 +56,6 @@ export PYTHONPATH=$PYTHONPATH:${WORK_DIR}
 #source ~/.bashrc
 #conda activate /mnt/qb/work/geiger/gwb629/conda/garage
 
-export OMP_NUM_THREADS=20  # Limits pytorch to spawn at most num cpus cores threads
+export OMP_NUM_THREADS=12  # Limits pytorch to spawn at most num cpus cores threads
 export OPENBLAS_NUM_THREADS=1  # Shuts off numpy multithreading, to avoid threads spawning other threads.
-python $WORK_DIR/tools/visualize_copycat.py --visualize-copycat 1 --number-of-workers 12 --pred-tuning-parameter 0.15 --gt-tuning-parameter 1 --baselines bcso bcoh arp
+python $WORK_DIR/tools/visualize_copycat.py --visualize-copycat 1 --number-of-workers 12 --pred-tuning-parameter 0.5 --loss-tuning-parameter 0.5 --baselines bcso bcoh arp
