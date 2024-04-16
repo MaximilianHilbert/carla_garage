dda48b2c9ecf9d71928396609cfb096570edf290
max <maximilian.hilbert@gmx.de>
2024-04-16 10:03:26 +0200

    jpg compress function

--- tools/compress.py
+++ tools/compress.py
@@ -1,12 +1,14 @@
 from PIL import Image
+from tqdm import tqdm
 import os
 for baseline in ["bcso", "bcoh", "arp"]:
     path=os.path.join("/home/maximilian-hilbert/carla_garage/vis", baseline)
-    for image_name in os.listdir(path):
+    path_new=os.path.join("/home/maximilian/Master/carla_garage/vis", "new", baseline)
+    os.makedirs(path_new, exist_ok=True)
+    for image_name in tqdm(os.listdir(path)):
         image = Image.open(os.path.join(path, image_name))
-
-
-        compressed_image = image.convert("RGB").save("output_compressed.png", optimize=True, quality=9)
+        rgb_im = image.convert('RGB')
+        rgb_im.save(os.path.join(path_new, image_name.replace(".png", ".jpg")), quality=95)
 
         # Close the image
         image.close()
