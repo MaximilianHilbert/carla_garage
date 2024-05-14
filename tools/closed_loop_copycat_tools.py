9aee43782c57e95e520aa12ef466d238702a5897
Maximilian <maximilian.hilbert@gmx.de>
2024-05-14 21:55:55 +0200

    additional logging closed loop

--- tools/closed_loop_copycat_tools.py
+++ tools/closed_loop_copycat_tools.py
@@ -22,21 +22,25 @@ def generate_cl_copycat_videos():
                     pass
                 residuals.update({file_path: residual})
     residual_values=np.array(list(residuals.values()))
+    
     mean=np.mean(residual_values)
     std=np.std(residual_values)
-    data_dict={"residuals": residual_values, "mean": mean, "std": std}
     
-    with open(os.path.join(main_root, "results.pkl"),"wb") as datafile:
-        pickle.dump(data_dict, datafile)
-    print("finished scanning...")
 
     threshold=mean-std*tuning_parameter_1
     print(f"current threshold marked as copycat is: {threshold}")
+    cc_count=0
     for index, (file, residual_value) in tqdm(enumerate(residuals.items())):
         if residual_value<threshold:
+            cc_count=+1
             rgb_folder=os.path.join(os.path.dirname(file), "with_rgb")
             generate_video(rgb_folder, save_path=os.path.join(main_root, "copycat_videos", f"{index}.avi"))
-            
+    data_dict={"num_collisions": len(residual_values), "num_cc_collisions":cc_count, "residuals": residual_values, "mean": mean, "std": std}
+    with open(os.path.join(main_root, "results.pkl"),"wb") as datafile:
+        pickle.dump(data_dict, datafile)
+    print(f"total collisions: {len(residual_values)}")
+    print(f"cc_only_collisions: {cc_count}")
+
 def main():
     generate_cl_copycat_videos()
 if __name__=="__main__":
