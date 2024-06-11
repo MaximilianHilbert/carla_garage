724442385de6e7958f92b1e039a2d3a7af464e20
Maximilian <maximilian.hilbert@gmx.de>
2024-06-11 19:06:20 +0200

    only inference on one weather

--- leaderboard/leaderboard/nocrash_evaluator.py
+++ leaderboard/leaderboard/nocrash_evaluator.py
@@ -490,7 +490,8 @@ class NoCrashEvaluator(object):
             "r",
         ) as f:
             routes = [tuple(map(int, l.split())) for l in f.readlines()]
-        weathers = {"train": [1, 6, 10, 14], "test": [3,8]}.get(args.weather)
+        #weathers = {"train": [1, 6, 10, 14], "test": [3,8]}.get(args.weather)
+        weathers = {"train": [14], "test": [8]}.get(args.weather)
         traffics = [0, 1, 2]
         for traffic, route, weather in itertools.product(traffics, routes, weathers):
             if self.statistics_manager.is_finished(args, route,weather, traffic):
