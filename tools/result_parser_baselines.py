c151322af2d9c3bc51e7c82f4d116bb9753d6928
Maximilian <maximilian.hilbert@gmx.de>
2024-06-11 14:30:49 +0200

    adapted result parser for new naming convention

--- tools/result_parser_baselines.py
+++ tools/result_parser_baselines.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import regex as re
 from tqdm import tqdm
+from coil_utils.baseline_helpers import get_ablations_dict
 weathers = {"train": [1, 3, 6, 8], "test": [10, 14]}
 
 
@@ -34,22 +35,19 @@ def main():
             eval_results["eval_rep"] = eval_rep
             df_lst.append(eval_results)
     df = pd.concat(df_lst, ignore_index=True)
-    df["success"] = (df["timeout"] == 0) & (df["collision"] == 0)
+    df["success"] = (df["timeout_blocked"] == 0) & (df["collision"] == 0)
     df["success"] = df["success"].astype("int")
     df["weather"] = df["weather"].map(lambda x: weather_mapping(x))
-    groups = df.groupby(["baseline", "experiment", "town","traffic", "weather", "eval_rep", "setting"]).agg(
-        timeouts_percentage=("timeout", "mean"),
-        collisions_percentage=("collision", "mean"),
-        success_percentage=("success", "mean"),
-    )
-    groups = groups.groupby(["baseline", "experiment", "town","traffic", "weather", "eval_rep", "setting"]).agg(
-        timeout_mean=("timeouts_percentage", "mean"),
-        timeout_std=("timeouts_percentage", "std"),
-        collisions_mean=("collisions_percentage", "mean"),
-        collisions_std=("collisions_percentage", "std"),
-        success_mean=("success_percentage", "mean"),
-        success_std=("success_percentage", "std"),
-    )
+    ablations_dict=get_ablations_dict()
+
+    groups = df.groupby(["baseline", "town", "traffic", "weather", *ablations_dict]).agg(
+        timeout_mean=("timeout_blocked", "mean"),
+        timeout_std=("timeout_blocked", "std"),
+        collisions_mean=("collision", "mean"),
+        collisions_std=("collision", "std"),
+        success_mean=("success", "mean"),
+        success_std=("success", "std"),
+        )
     groups.to_csv(os.path.join(os.environ.get("RESULT_ROOT"), os.path.join(os.environ.get("RESULT_ROOT"),"combined.csv")))
 
 
