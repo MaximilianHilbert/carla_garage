ec82a6edf3560f0095d85598245ed12c9aa3b951
Maximilian <maximilian.hilbert@gmx.de>
2024-07-02 00:25:04 +0200

    small lr test

--- team_code/config.py
+++ team_code/config.py
@@ -50,7 +50,7 @@ class GlobalConfig:
         self.bev_height=256
         self.bev_width=256
         #use self.learning_rate = 0.001 for debugging with batchsize 10 on one_sample
-        self.learning_rate_single_obs = 5e-4
+        self.learning_rate_single_obs = 1e-5
         self.learning_rate_multi_obs=1e-4
         self.reduced_channel_dimension=256
         self.use_color_aug= 0
