fixed closed loop normalization

coil_utils/baseline_helpers.py
@@ -206,10 +206,9 @@ def visualize_model(  # pylint: disable=locally-disabled, unused-argument
     loss_brake=None,
     loss_velocity=None
 ):
-    if config.normalize_imagenet:
-        rgb=t_u.unnormalize_imagenet(rgb)
-    else:
-        rgb=rgb*255.0
+    
+    rgb=t_u.normalization_wrapper(x=rgb, dataset_name=config.normalization_strategy, type="unnormalize")
+    
     # 0 Car, 1 Pedestrian, 2 Red light, 3 Stop sign
     color_classes = [
         np.array([144, 238, 144]),

team_code/coil_agent.py
@@ -183,11 +183,8 @@ class CoILAgent(AutonomousAgent):
             control.throttle = 0.0
             current_image = current_data.get("CentralRGB")[1][..., :3]
             current_image = cv2.cvtColor(current_image, cv2.COLOR_BGR2RGB)
-            current_image=torch.from_numpy(current_image).type(torch.FloatTensor).cuda()
-            if self.config.normalize_imagenet:
-                current_image=t_u.normalize_imagenet(current_image)
-            else:
-                current_image=current_image/255.0
+            current_image=torch.from_numpy(current_image).type(torch.FloatTensor)
+            current_image=t_u.normalization_wrapper(x=current_image,dataset_name=self.config.normalization_strategy, type="normalize")
             current_speed=current_data.get("speed")[1]["speed"]
             current_position = self.vehicle.get_location()
             current_position = np.array([current_position.x, current_position.y])
@@ -266,12 +263,8 @@ class CoILAgent(AutonomousAgent):
         # Conversion to old convention necessary in carla >=0.9, only take BGR Values without alpha channel and convert to RGB for the model
         current_image = sensor_data.get("CentralRGB")[1][..., :3]
         current_image = cv2.cvtColor(current_image, cv2.COLOR_BGR2RGB)
-        
-        if self.config.normalize_imagenet:
-            current_image=torch.from_numpy(current_image).type(torch.FloatTensor).cuda()
-            current_image = t_u.normalize_imagenet(current_image)
-        else:
-            current_image = current_image/255.0
+        current_image=torch.from_numpy(current_image).type(torch.FloatTensor)
+        current_image=t_u.normalization_wrapper(x=current_image, dataset_name=self.config.normalization_strategy, type="normalize")
         vehicle_speed=sensor_data.get("speed")[1]["speed"]
         #add data to queues in beginning of the loop, so we have historical information + current information and then subsample before it comes into the model
         # here we add the current one but the self.prev_location_queue does append later, we fuse only historical locations, but also current speeds and observations