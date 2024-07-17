edf605c433b6b0769dfdaac42d9967aad794f234
Maximilian <maximilian.hilbert@gmx.de>
2024-07-17 14:25:14 +0200

    small bug fix

--- evaluate_nocrash_baselines.py
+++ evaluate_nocrash_baselines.py
@@ -15,10 +15,6 @@ def main(args):
     for ablation, value in get_ablations_dict().items():
         if ablation not in config.__dict__:
             setattr(config, ablation, value)
-    if "sampling_rate" not in config.__dict__:
-        setattr(config, "sampling_rate", 1)
-    setattr(config, "max_img_seq_len_baselines", 3)
-    setattr(config, "speed", 0)
 
     runner = NoCrashEvalRunner(args, config,town, weather, port=port, tm_port=args.tm_port, debug=debug)
     runner.run()
