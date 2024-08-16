06ffeaf3cdb2f611cc5bf729cbc4aa9435ef23c2
Maximilian Hilbert <maximilian.hilbert@gmx.de>
2024-08-16 15:55:47 +0200

    changed debugging sh file

--- leaderboard/scripts/train_nocrash_baselines_single.sh
+++ leaderboard/scripts/train_nocrash_baselines_single.sh
@@ -25,21 +25,21 @@
 # export LD_LIBRARY_PATH="/home/hilbert/miniconda3/envs/garage/lib":$LD_LIBRARY_PATH
 
 #uni pc
-export WORK_DIR=/home/maximilian-hilbert/carla_garage
-export CONFIG_ROOT=${WORK_DIR}/coil_configuration
-export CARLA_ROOT=${WORK_DIR}/carla
-export DATASET_ROOT=/home/maximilian-hilbert/datasets/tf_dataset
-export LD_LIBRARY_PATH="/mnt/lustre/work/geiger/gwb629/conda/garage/lib":$LD_LIBRARY_PATH
-export TEAM_CODE=$WORK_DIR/team_code
-export COIL_NETWORK=${WORK_DIR}/coil_network
-#mlcloud
-# export WORK_DIR=/mnt/lustre/work/geiger/gwb629/carla_garage
+# export WORK_DIR=/home/maximilian-hilbert/carla_garage
 # export CONFIG_ROOT=${WORK_DIR}/coil_configuration
 # export CARLA_ROOT=${WORK_DIR}/carla
-# export DATASET_ROOT=/mnt/lustre/work/geiger/bjaeger25/old_repos/datasets/hb_dataset_v08_2023_05_10
+# export DATASET_ROOT=/home/maximilian-hilbert/datasets/tf_dataset
 # export LD_LIBRARY_PATH="/mnt/lustre/work/geiger/gwb629/conda/garage/lib":$LD_LIBRARY_PATH
 # export TEAM_CODE=$WORK_DIR/team_code
 # export COIL_NETWORK=${WORK_DIR}/coil_network
+#mlcloud
+export WORK_DIR=/mnt/lustre/work/geiger/gwb629/carla_garage
+export CONFIG_ROOT=${WORK_DIR}/coil_configuration
+export CARLA_ROOT=${WORK_DIR}/carla
+export DATASET_ROOT=/mnt/lustre/work/geiger/gwb629/datasets/routewise_augmentation_rear_camera2024_08_10
+export LD_LIBRARY_PATH="/mnt/lustre/work/geiger/gwb629/conda/garage/lib":$LD_LIBRARY_PATH
+export TEAM_CODE=$WORK_DIR/team_code
+export COIL_NETWORK=${WORK_DIR}/coil_network
 
 export CARLA_SERVER=${CARLA_ROOT}/CarlaUE4.sh
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
@@ -52,18 +52,18 @@ export PYTHONPATH=$PYTHONPATH:${TEAM_CODE}
 export PYTHONPATH=$PYTHONPATH:${WORK_DIR}
 
 #mlcloud
-# source ~/.bashrc
-# conda activate /mnt/lustre/work/geiger/gwb629/conda/garage
+source ~/.bashrc
+conda activate /mnt/lustre/work/geiger/gwb629/conda/garage
 #tcml
 # source /home/hilbert/.bashrc
 # eval "$(conda shell.bash hook)"
 # conda activate garage
-export OMP_NUM_THREADS=12  # Limits pytorch to spawn at most num cpus cores threads
+export OMP_NUM_THREADS=64  # Limits pytorch to spawn at most num cpus cores threads
 export OPENBLAS_NUM_THREADS=1  # Shuts off numpy multithreading, to avoid threads spawning other threads.
-torchrun --nnodes=1 --nproc_per_node=1 --rdzv_id=100 \
+torchrun --nnodes=1 --nproc_per_node=8 --rdzv_id=100 \
  --rdzv_backend=c10d $TEAM_CODE/coil_train.py --seed 10214 \
- --baseline-folder-name bcoh --number-of-workers 12 \
- --batch-size 2 \
+ --baseline-folder-name arp --number-of-workers 8 \
+ --batch-size 10 \
  --bev 1 --detectboxes 1 --predict_vectors 1 \
- --training-repetition 0 --backbone swin \
+ --training-repetition 0 --backbone resnet \
  --experiment-id id_000
