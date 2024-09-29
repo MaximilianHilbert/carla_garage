339104cb9bdd3cf4463e5028dc0d7c789001c2ee
Maximilian Hilbert <maximilian.hilbert@gmx.de>
2024-09-29 14:22:12 +0200

    minor fix

--- train_nocrash_baselines.py
+++ train_nocrash_baselines.py
@@ -229,7 +229,7 @@ torchrun --nnodes=1 --nproc_per_node=8 --rdzv_id=100 --rdzv_backend=c10d $TEAM_C
    
     training_finished = False
     while not training_finished:
-        num_running_jobs, max_num_parallel_jobs = get_num_jobs(job_name=experiment_name_stem, username=username)
+        num_running_jobs, max_num_parallel_jobs = get_num_jobs(code_root=code_root,job_name=experiment_name_stem, username=username)
         print(f"{num_running_jobs} jobs are running...")
         time.sleep(10)
 
